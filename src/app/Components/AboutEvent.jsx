7662ee3d2279eaf9ab08b9e9a21306bf4309f1ab
SANCHARI KARATI <sancharik42000@gmail.com>
2024-08-28 16:25:33 +0530

    About Event

---
+++ src/app/Components/AboutEvent.jsx
@@ -0,0 +1,100 @@
+const AboutEvent = () => {
+    return (
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-20 lg:px-5 lg:py-50 bg-[url('/bg2.jpg')] bg-cover flex flex-col items-center justify-center">
+        <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+          <div>
+            <p className="inline-block px-3 py-px mb-4 text-sm font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+              Kodikas Game Plan
+            </p>
+          </div>
+          <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+            <span className="relative inline-block">
+              <svg
+                viewBox="0 0 52 24"
+                fill="currentColor"
+                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+              >
+              </svg>
+              <button className="px-8 z-30 py-4 bg-slate-950 rounded-md text-white relative font-semibold font-sans after:-z-20 after:absolute after:h-1 after:w-1 after:bg-zinc-700 after:left-5 overflow-hidden after:bottom-0 after:translate-y-full after:rounded-md after:hover:scale-[300] after:hover:transition-all after:hover:duration-700 after:transition-all after:duration-700 transition-all duration-700 [text-shadow:3px_5px_2px_#be123c;] hover:[text-shadow:2px_2px_2px_#fda4af] text-2xl">
+              ROUND DETAILS 
+              </button>
+            </span>{' '}
+          </h2>
+          <p className="text-base text-gray-200 md:text-lg">
+            Participants are requested to go through the round details.
+          </p>
+        </div>
+        <div className="grid max-w-sm gap-5 mb-8 lg:grid-cols-3 sm:mx-auto lg:max-w-full">
+          <div className="px-10 py-20 text-center lg:px-5 lg:py-10 xl:py-20 group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg">
+            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-200 uppercase">
+              19 September
+            </p>
+            <a
+              href="/"
+              className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
+              title="Round 1"
+            >
+              Round 1 
+            </a>
+            <br></br>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Round 1 Details
+            </a>
+          </div>
+          <div className="px-10 py-20 text-center lg:px-5 lg:py-10 xl:py-20 group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg">
+            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
+              20 Nov 2020
+            </p>
+            <a
+              href="/"
+              className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
+              aria-label="Read article"
+              title="Well, the way they make shows is, they make one"
+            >
+              Well, the way they make shows is, they make one
+            </a>
+            <p className="max-w-xs mx-auto mb-2 text-gray-700">
+              Some pilots get picked and become television programs. Some don't,
+              become nothing.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Read more
+            </a>
+          </div>
+          <div className="px-10 py-20 text-center lg:px-5 lg:py-10 xl:py-20 group group-hover:before:duration-500 group-hover:after:duration-500 after:duration-500 hover:border-rose-300 hover:before:[box-shadow:_20px_20px_20px_30px_#a21caf] duration-500 before:duration-500 hover:duration-500 hover:after:-right-8 hover:before:right-12 hover:before:-bottom-8 hover:before:blur origin-left hover:decoration-2 hover:text-rose-300 relative bg-neutral-800 border text-left p-3 text-gray-50 text-base font-bold rounded-lg  overflow-hidden  before:absolute before:w-12 before:h-12 before:content[''] before:right-1 before:top-1 before:z-10 before:bg-violet-500 before:rounded-full before:blur-lg  after:absolute after:z-10 after:w-20 after:h-20 after:content['']  after:bg-rose-300 after:right-8 after:top-3 after:rounded-full after:blur-lg">
+            <p className="mb-2 text-xs font-semibold tracking-wide text-gray-600 uppercase">
+              20 Nov 2020
+            </p>
+            <a
+              href="/"
+              className="inline-block max-w-xs mx-auto mb-3 text-2xl font-extrabold leading-7 transition-colors duration-200 hover:text-deep-purple-accent-400"
+              aria-label="Read article"
+              title="Pommy ipsum smeg head whizz morris himer due"
+            >
+              Pommy ipsum smeg head whizz morris himer due
+            </a>
+            <p className="max-w-xs mx-auto mb-2 text-gray-700">
+              Taking the mick middle className bog roll bow ties are cool posh
+              nosh off t'shop, stew and dumps.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Read more
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  };
+  export default AboutEvent;
--- src/app/Components/Header/Heaader.jsx
+++ src/app/Components/Header/Heaader.jsx
@@ -12,12 +12,15 @@ const Header = () => {
             </p>
           </div>
           <div>
-            <a
-              href="#about-event"
-              className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-gray-900 hover:bg-gray-700 focus:shadow-outline focus:outline-none"
-            >
-              Explore now
+            <button className="overflow-hidden relative w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer relative z-10 group">
+            <a href="#about-event">
+            <span className="absolute w-36 h-32 -top-8 -left-2 bg-zinc-200 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-right"></span>
+            <span className="absolute w-36 h-32 -top-8 -left-2 bg-zinc-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-right"></span>
+            <span className="absolute w-36 h-32 -top-8 -left-2 bg-zinc-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-right"></span>
+              Click Here
+            <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">Explore!</span>
             </a>
+            </button>
           </div>
         </div>
       </div>
