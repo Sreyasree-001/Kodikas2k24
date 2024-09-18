55d04aa8b4f33ac831e7acfe1d89ec6066710a7e
Sreyasree Sasmal <sreyasree202@gmail.com>
2024-09-19 01:41:02 +0530

    fix: Registration page

--- src/app/Components/Footer.jsx
+++ src/app/Components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="bg-black">
-        <div className="px-4 pt-16 mb-4 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="px-4 pt-16 mb-4 md:mb-0 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
           <div className="flex flex-col justify-between pt-5 pb-10 border-t border-gray-800 sm:flex-row">
             <p className="text-sm text-gray-500">
               © Copyright Kodikas 2k24. All rights reserved.
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -5,10 +5,12 @@ import Navbar from "../Components/Header/Navbar/Navbar";
 import Footer from "../Components/Footer";
 import { baseURL } from "../baseURL";
 import axios from "axios";
+import { BarLoader, DotLoader } from "react-spinners";
 
 export default function register() {
   const [data, setData] = useState({});
   //const [uid, setUid] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (e) => {
     let name = e.target.name;
@@ -18,6 +20,7 @@ export default function register() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     console.log(data);
     try {
       let resp = await axios({
@@ -32,222 +35,245 @@ export default function register() {
       setData({});
     } catch (err) {
       if (err.response.status == 500)
-        alert("Team name already exists\n Try something else!");
+        alert("Team name already exists or invalid data\n Try something else!");
       else console.log(err);
     }
+    setLoading(false);
   };
 
   return (
     <>
-     <div className="pt-16 w-full min-h-screen z-5 bg-[url('/bg4.jpg')] bg-cover bg-center bg-no-repeat 
-    flex flex-col items-center justify-center">
-      <Navbar />
-      <main className=" bg-transparent flex flex-col items-center justify-center min-h-screen py-10 w-2/3">
-        <div className=" w-full mx-auto relative overflow-hidden z-10 bg-[#0a0a0a] p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
-          <h2 className="text-2xl font-bold text-white mb-6">Register Here</h2>
-          <form >
-            <div className="mb-4">
-              <label
-                htmlFor="username"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Team Name
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="username"
-                type="text"
-                name="teamName"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <div className="grid md:grid-cols-2 gap-4">
-            <div>
-            <div>
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Team Leader Name{" "}
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="name1"
-                type="text"
-                name="firstMemName"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <br></br>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Email
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="email1"
-                type="email"
-                name="firstMemEmail"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <br></br>
-            <div>
-              <label
-                htmlFor="class"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Department
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="class"
-                type="text"
-                name="firstMemClass"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <br></br>
-            <div>
-              <label
-                htmlFor="roll"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Class Roll no.
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="roll"
-                type="number"
-                name="firstMemRoll"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <br></br>
-            <div>
-              <label
-                htmlFor="phone"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Phone No
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="phone1"
-                type="number"
-                name="firstMemPh"
-                onChange={handleChange}
-              ></input>
-            </div>
-            </div>
-            
-
-            <div>
-            <div>
-              <label
-                htmlFor="name2"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Teammate Name
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="name2"
-                type="text"
-                name="secMemName"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <br></br>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Email
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="email2"
-                type="email"
-                name="secMemEmail"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <br></br>
-            <div>
-              <label
-                htmlFor="class"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Department
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="class"
-                type="text"
-                name="secMemClass"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <br></br>
-            <div>
-              <label
-                htmlFor="roll"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Class Roll no.
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="roll"
-                type="number"
-                name="secMemRoll"
-                onChange={handleChange}
-              ></input>
-            </div>
-            <br></br>
-            <div>
-              <label
-                htmlFor="phone"
-                className="block text-sm font-medium text-gray-300"
-              >
-                Phone No
-              </label>
-              <input
-                className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
-                id="phone2"
-                type="number"
-                name="secMemPh"
-                onChange={handleChange}
-              ></input>
-            </div>
-            </div>
-            </div>
-            <br></br>
-            <button
-              className="group relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
-              type="submit"
-              onClick={handleSubmit}
-            >
-              <span className="relative px-5 py-2.5 transition-all ease-in duration-200 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Register
-              </span>
-              <div className="ease-in duration-300 opacity-0 group-hover:block group-hover:opacity-100 transition-all">
-                <div className="ease-in-out duration-500 -translate-y-4 pointer-events-none transition-all group-hover:-translate-y-16 absolute left-1/2 z-50 flex -translate-x-1/2 flex-col items-center rounded-sm text-center text-sm text-slate-300 before:-top-2">
-                  <div className="rounded-sm bg-black py-1 px-2">
-                    <p className="whitespace-nowrap">Click Here!</p>
+      <div
+        className="pt-16 w-full min-h-screen z-5 bg-[url('/bg4.jpg')] bg-cover bg-center bg-no-repeat 
+    flex flex-col items-center justify-center"
+      >
+        <Navbar />
+        <main className=" bg-transparent flex flex-col items-center justify-center min-h-screen py-10 w-2/3">
+          <div className=" w-full mx-auto relative overflow-hidden z-10 bg-[#0a0a0a] p-8 rounded-lg shadow-md before:w-24 before:h-24 before:absolute before:bg-purple-600 before:rounded-full before:-z-10 before:blur-2xl after:w-32 after:h-32 after:absolute after:bg-sky-400 after:rounded-full after:-z-10 after:blur-xl after:top-24 after:-right-12">
+            <h2 className="text-2xl font-bold text-white mb-6">
+              Register Here
+            </h2>
+            <form>
+              <div className="mb-4">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-medium text-gray-300"
+                >
+                  Team Name
+                </label>
+                <input
+                  className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                  id="username"
+                  type="text"
+                  name="teamName"
+                  onChange={handleChange}
+                  required
+                ></input>
+              </div>
+              <div className="grid md:grid-cols-2 gap-4">
+                <div>
+                  <div>
+                    <label
+                      htmlFor="name"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Team Leader Name{" "}
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="name1"
+                      type="text"
+                      name="firstMemName"
+                      onChange={handleChange}
+                      required
+                    ></input>
                   </div>
-                  <div className="h-0 w-fit border-l-8 border-r-8 border-t-8 border-transparent border-t-black"></div>
+                  <br></br>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="email1"
+                      type="email"
+                      name="firstMemEmail"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
+                  <br></br>
+                  <div>
+                    <label
+                      htmlFor="class"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Department
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="class"
+                      type="text"
+                      name="firstMemClass"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
+                  <br></br>
+                  <div>
+                    <label
+                      htmlFor="roll"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Class Roll no.
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="roll"
+                      type="number"
+                      name="firstMemRoll"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
+                  <br></br>
+                  <div>
+                    <label
+                      htmlFor="phone"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Phone No
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="phone1"
+                      type="number"
+                      name="firstMemPh"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
+                </div>
+
+                <div>
+                  <div>
+                    <label
+                      htmlFor="name2"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Teammate Name
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="name2"
+                      type="text"
+                      name="secMemName"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
+                  <br></br>
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="email2"
+                      type="email"
+                      name="secMemEmail"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
+                  <br></br>
+                  <div>
+                    <label
+                      htmlFor="class"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Department
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="class"
+                      type="text"
+                      name="secMemClass"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
+                  <br></br>
+                  <div>
+                    <label
+                      htmlFor="roll"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Class Roll no.
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="roll"
+                      type="number"
+                      name="secMemRoll"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
+                  <br></br>
+                  <div>
+                    <label
+                      htmlFor="phone"
+                      className="block text-sm font-medium text-gray-300"
+                    >
+                      Phone No
+                    </label>
+                    <input
+                      className="mt-1 p-2 w-full bg-[#333030] border border-gray-600 rounded-md text-white"
+                      id="phone2"
+                      type="number"
+                      name="secMemPh"
+                      onChange={handleChange}
+                      required
+                    ></input>
+                  </div>
                 </div>
               </div>
-            </button>
-          </form>
-        </div>
-      </main>
+              <br></br>
+
+              {loading ? (
+                <BarLoader type="ThreeDots" color="#3E195E" className="rounded p-2 w-72"/>
+              ) : (
+                <>
+                  <button
+                    className="group relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800"
+                    type="submit"
+                    onClick={handleSubmit}
+                    disabled={loading}
+                  >
+                    <span className="relative px-5 py-2.5 transition-all ease-in duration-200 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                      Register
+                    </span>
+                    <div className="ease-in duration-300 opacity-0 group-hover:block group-hover:opacity-100 transition-all">
+                      <div className="ease-in-out duration-500 -translate-y-4 pointer-events-none transition-all group-hover:-translate-y-16 absolute left-1/2 z-50 flex -translate-x-1/2 flex-col items-center rounded-sm text-center text-sm text-slate-300 before:-top-2">
+                        <div className="rounded-sm bg-black py-1 px-2">
+                          <p className="whitespace-nowrap">Click Here!</p>
+                        </div>
+                        <div className="h-0 w-fit border-l-8 border-r-8 border-t-8 border-transparent border-t-black"></div>
+                      </div>
+                    </div>
+                  </button>
+                </>
+              )}
+            </form>
+          </div>
+        </main>
       </div>
       <Footer />
     </>
