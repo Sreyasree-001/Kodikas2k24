1f4df04a9af9efbd7e3ed5d333a29fd5826794bd
Arka-Laha <98916010+Arka-Laha@users.noreply.github.com>
2024-09-19 04:09:53 +0530

    Fix

--- src/app/Components/Header/Heaader.jsx
+++ src/app/Components/Header/Heaader.jsx
@@ -19,7 +19,7 @@ const Header = () => {
                 <span className="absolute w-36 h-32 -top-8 -left-2 bg-zinc-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-right"></span>
                 <span className="absolute w-36 h-32 -top-8 -left-2 bg-zinc-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-right"></span>
                 Click Here
-                <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">Explore!</span>
+                <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10">Register!</span>
               </a>
             </button>
           </div>
