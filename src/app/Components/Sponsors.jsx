3630eae837fde50c1f7d2b776804dee8ad4cdc56
ArijitKRoy <83859073+arijitkroy@users.noreply.github.com>
2024-09-25 20:24:31 +0530

    added and updated sponsors

--- src/app/Components/Sponsors.jsx
+++ src/app/Components/Sponsors.jsx
@@ -1,91 +1,85 @@
 export default function Sponsors() {
     return (
-        
-        <div id="sponsors" className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
-          <button className="group relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
-          <span
-             className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-             Sponsors
-          </span>
-        <div class="hidden group-hover:block">
-        <div
-        className="group absolute -top-12 left-1/2 z-50 flex -translate-x-1/2 flex-col items-center rounded-sm text-center text-sm text-slate-300 before:-top-2">
-      <div className="rounded-sm bg-black py-1 px-2">
-        <p className="whitespace-nowrap">Of KODIKAS 2024</p>
-      </div>
-      <div
-        className="h-0 w-fit border-l-8 border-r-8 border-t-8 border-transparent border-t-black"></div>
-      </div>
-      </div>
-          </button>
-          <p className="text-base text-gray-700 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium.
+      <>
+      <div id="committee" className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
+          <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-500 uppercase rounded-full bg-teal-accent-400">
+            Discover Our Event Sponsors
           </p>
         </div>
-        <div className="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
-          <div>
-            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
-              <img
-                className="absolute object-cover w-full h-full rounded"
-                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=3&amp;h=750&amp;w=1260"
-                alt="Person"
-              />
-            </div>
-            <div className="flex flex-col sm:text-center">
-              <p className="text-lg text-gray-100 font-bold">Oliver Aguilerra</p>
-              <p className="mb-5 text-xs text-gray-300">Product Manager</p>
-              <div className="flex items-center space-x-3 sm:justify-center">
-              </div>
-            </div>
+        <div className="flex gap-3 flex-wrap">
+          <a class="p-8 max-w-sm border border-indigo-300 rounded-2xl hover:shadow-xl hover:shadow-indigo-50 flex flex-col items-center"
+          href="https://www.ardentcollaborations.com">
+          <img src="/Sponsors/ardent.jpg" class="shadow rounded-lg overflow-hidden border" />
+          <div class="mt-8">
+              <h4 class="font-bold text-gray-400 text-xl">Ardent Computech Pvt. Ltd.</h4>
           </div>
-          <div>
-            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
-              <img
-                className="absolute object-cover w-full h-full rounded"
-                src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt="Person"
-              />
-            </div>
-            <div className="flex flex-col sm:text-center">
-              <p className="text-lg text-gray-100 font-bold">Marta Clermont</p>
-              <p className="mb-5 text-xs text-gray-300">Design Team Lead</p>
-              <div className="flex items-center space-x-3 sm:justify-center">
-            </div>
-            </div>
+      </a>
+      <a class="p-8 max-w-sm border border-indigo-300 rounded-2xl hover:shadow-xl hover:shadow-indigo-50 flex flex-col items-center"
+          href="https://www.pw.live">
+          <img src="/Sponsors/pw.jpg" class="shadow rounded-lg overflow-hidden border" />
+          <div class="mt-8">
+              <h4 class="font-bold text-gray-400 text-xl">Physics Wallah</h4>
           </div>
-          <div>
-            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
-              <img
-                className="absolute object-cover w-full h-full rounded"
-                src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt="Person"
-              />
-            </div>
-            <div className="flex flex-col sm:text-center">
-              <p className="text-lg text-gray-100 font-bold">Alice Melbourne</p>
-              <p className="mb-5 text-xs text-gray-300">Human Resources</p>
-              <div className="flex items-center space-x-3 sm:justify-center">
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
-              <img
-                className="absolute object-cover w-full h-full rounded"
-                src="https://images.pexels.com/photos/3931603/pexels-photo-3931603.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                alt="Person"
-              />
-            </div>
-            <div className="flex flex-col sm:text-center">
-              <p className="text-lg text-gray-100 font-bold">John Doe</p>
-              <p className="mb-5 text-xs text-gray-300">Good guy</p>
-              <div className="flex items-center space-x-3 sm:justify-center">
-              </div>
-            </div>
-          </div>
+      </a>
         </div>
       </div>
+      </>
+        
+        
+      //   <div id="sponsors" className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      //       <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
+      //       <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-500 uppercase rounded-full bg-teal-accent-400">
+      //         Discover Our Sponsors
+      //       </p>
+      //     </div>
+      //   <div className="grid gap-10 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
+      //     <div>
+      //       <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+      //         <img
+      //           className="absolute object-cover w-full h-full rounded"
+      //           src="/Sponsors/ardent.jpg"
+      //           alt="Person"
+      //         />
+      //       </div>
+      //       <div className="flex flex-col sm:text-center">
+      //         <p className="text-lg text-gray-100 font-bold">Ardent Computech Pvt Ltd</p>
+      //         <p className="mb-5 text-xs text-gray-300">Product Manager</p>
+      //         <div className="flex items-center space-x-3 sm:justify-center">
+      //         </div>
+      //       </div>
+      //     </div>
+      //     <div>
+      //       <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+      //         <img
+      //           className="absolute object-cover w-full h-full rounded"
+      //           src="https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+      //           alt="Person"
+      //         />
+      //       </div>
+      //       <div className="flex flex-col sm:text-center">
+      //         <p className="text-lg text-gray-100 font-bold">Marta Clermont</p>
+      //         <p className="mb-5 text-xs text-gray-300">Design Team Lead</p>
+      //         <div className="flex items-center space-x-3 sm:justify-center">
+      //       </div>
+      //       </div>
+      //     </div>
+      //     <div>
+      //       <div className="relative pb-56 mb-4 rounded shadow lg:pb-64">
+      //         <img
+      //           className="absolute object-cover w-full h-full rounded"
+      //           src="https://images.pexels.com/photos/3747435/pexels-photo-3747435.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+      //           alt="Person"
+      //         />
+      //       </div>
+      //       <div className="flex flex-col sm:text-center">
+      //         <p className="text-lg text-gray-100 font-bold">Alice Melbourne</p>
+      //         <p className="mb-5 text-xs text-gray-300">Human Resources</p>
+      //         <div className="flex items-center space-x-3 sm:justify-center">
+      //         </div>
+      //       </div>
+      //     </div>
+      //   </div>
+      // </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
         <AboutUs/>
         <Gallery/>
         <Comittees/>
-        {/* <Sponsors/> */}
+        <Sponsors/>
         <Contact/>
         <Footer/>
     </div>
